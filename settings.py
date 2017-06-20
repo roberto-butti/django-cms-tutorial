a21eaa42522ca7d3aa64cf49378feebc45bc25c6
Roberto Butti <rbutti@h-art.com>
2017-06-20 16:13:46 +0000

    install aldryn-bootstrap3 1.2.2

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ INSTALLED_ADDONS = [
     'aldryn-sso',
     'aldryn-django-cms',
     'aldryn-devsync',
+    'aldryn-bootstrap3',
     'djangocms-file',
     'djangocms-googlemap',
     'djangocms-history',
