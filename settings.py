0e77d59643c9f111b4ec645a6c4d7ce7a8e54a24
Roberto Butti <rbutti@h-art.com>
2017-06-20 16:16:23 +0000

    uninstall djangocms-picture 2.0.5

--- settings.py
+++ settings.py
@@ -11,7 +11,6 @@ INSTALLED_ADDONS = [
     'aldryn-newsblog',
     'djangocms-googlemap',
     'djangocms-history',
-    'djangocms-picture',
     'djangocms-snippet',
     'djangocms-style',
     'djangocms-text-ckeditor',
